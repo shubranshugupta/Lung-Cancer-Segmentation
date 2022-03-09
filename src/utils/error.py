ed64378c2b379ca133ea2cdd3084abbfd37a2b7a
shubranshugupta <57756826+shubranshugupta@users.noreply.github.com>
2022-03-09 19:14:44 +0530

    added data download error

--- src/utils/error.py
+++ src/utils/error.py
@@ -9,3 +9,9 @@ class AWSCredentialError(Exception):
         self.message = message
     def __str__(self):
         return self.message
+
+class DownloadDataError(Exception):
+    def __init__(self, message):
+        self.message = message
+    def __str__(self):
+        return self.message
